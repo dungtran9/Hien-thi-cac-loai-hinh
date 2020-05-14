dff18b54afce7650a70b70ee29b001f5a4307d8c
Dung <vietdung.rx@gmail.com>
2020-05-14 21:48:25 +0700

    update

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<p>Menu</p>
+<a href="Rectangle.php">1.Print the rectangle</a><br>
+<a href="square-triangle.php">2.Print the square triangle </a><br>
+<a href="isoscesles-triangle.php">3.Print isosceles triangle</a><br>
+<a href="index.php">Exit</a>
+</body>
+</html>
